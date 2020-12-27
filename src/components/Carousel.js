fb14e716a20291d6a298819528bf6b9e18765a61
Suresh <C5110355@acsind.com>
2020-12-27 11:02:59 +0530

    hosted images online

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,21 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "react-responsive-carousel/lib/styles/carousel.min.css"
 import { Carousel } from 'react-responsive-carousel'
+import 'firebase/storage'
+import { firebase } from '../firebase/firebase'
 
 
 const CarouselPage = () => {
+
+    const [img1, setImg1] = useState()
+    const [img2, setImg2] = useState()
+    const [img3, setImg3] = useState()
+
+    useEffect(() => {
+        const storage = firebase.storage()
+        storage.ref('/images/carousel/1.jpg').getDownloadURL()
+            .then(url => setImg1(url))
+        storage.ref('/images/carousel/2.jpg').getDownloadURL()
+            .then(url => setImg2(url))
+        storage.ref('/images/carousel/3.jpg').getDownloadURL()
+            .then(url => setImg3(url))
+    }, [])
 
     return (
         <div className="content__carousel">
             <Carousel autoPlay infiniteLoop showStatus={false} showThumbs={false} stopOnHover={false} interval={4000}>
                 <div>
-                    <img src="/images/carousel/1.jpg" alt="img1" className='content__img' />
+                    <img src={img1} alt="img1" className='content__img' />
                 </div>
                 <div>
-                    <img src="/images/carousel/2.jpg" alt="img2" className='content__img' />
+                    <img src={img2} alt="img2" className='content__img' />
                 </div>
                 <div>
-                    <img src="/images/carousel/3.jpg" alt="img3" className='content__img' />
+                    <img src={img3} alt="img3" className='content__img' />
                 </div>
             </Carousel>
         </div>
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,13 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Gallery from 'react-grid-gallery';
-import { images } from './GalleryImages'
+import 'firebase/storage'
+import { firebase } from '../firebase/firebase'
+import Loader from '../components/Loader'
+
 
 const Portfolio = () => {
+
+    const storage = firebase.storage()
+    const [images, setImages] = useState([])
+    const [loading, setLoading] = useState(false)
+    
+
+    useEffect(() => {
+        storage.ref('/config/galleryimages.json').getDownloadURL()
+            .then((url) => {
+                setLoading(true)
+                fetch(url,{
+                    method: 'GET'                    
+                })
+                    .then(response => response.json())
+                    .then(data => {
+                        setImages(data)
+                        setLoading(false)
+                    })
+                    .catch(err => {
+                        console.log(err)
+                        setLoading(false)
+                    })
+            })
+    },[storage])
 
     return (
         <div className='content anchor' id="portfolio">
             <h2 className='content__header'>Portfolio</h2>
-            <Gallery images={images} />
+            {loading ? <Loader /> : <Gallery images={images} />}
         </div>
     )
 }
