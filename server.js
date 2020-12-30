5becd92edd599adb36ef08eb95ce22c38e9c103f
Suresh <C5110355@acsind.com>
2020-12-30 10:18:40 +0530

    added compression to server

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express');
+const compression = require('compression');
 const path = require('path');
-const port = process.env.PORT || 3000;
 const app = express();
-
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
+ 
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
-  return res.send('pong');
+ 
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-app.listen(port, () => {
-    console.log('App is running')
+ 
+const PORT = process.env.PORT || 3000;
+ 
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}`);
 });
